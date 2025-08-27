14d40f97317339a4610cc35bfa04460ad45f21aa
Yudnikov-Leonid <dorhungmi2@gmail.com>
2025-08-28 00:53:16 +0300

    add pictures to full descriptions

--- lib/features/point_details/point_full_details_screen.dart
+++ lib/features/point_details/point_full_details_screen.dart
@@ -10,47 +10,63 @@ class PointFullDetailsScreen extends StatelessWidget {
   final RouteSinglePointData point;
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: TripleAppBar(
-      context,
-      title: point.pointName,
-      leadingFunction: () {
-        AppNavigator.pop(context);
-      },
-    ),
-    body: Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(height: 8),
-            Text(
-              point.fullDescription,
-              textAlign: TextAlign.justify,
-              style: context.textTheme.bodyLarge?.copyWith(
-                fontSize: 18,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            const SizedBox(height: 16),
-            if (point.fullDescription.isNotEmpty)
-              TextButton(
-                onPressed: () {
-                  AppNavigator.pop(context);
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                  foregroundColor: Colors.grey.shade600,
+  Widget build(BuildContext context) {
+    final split = point.fullDescription.split('||');
+
+    return Scaffold(
+      appBar: TripleAppBar(
+        context,
+        title: point.pointName,
+        leadingFunction: () {
+          AppNavigator.pop(context);
+        },
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 8),
+              ...split.map((e) {
+                if (e.startsWith('image:')) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: Center(
+                      child: Image.asset('assets/images/${e.substring(6)}'),
+                    ),
+                  );
+                }
+
+                return Text(
+                  e,
+                  textAlign: TextAlign.justify,
+                  style: context.textTheme.bodyLarge?.copyWith(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                  ),
+                );
+              }).toList(),
+              const SizedBox(height: 16),
+              if (point.fullDescription.isNotEmpty)
+                TextButton(
+                  onPressed: () {
+                    AppNavigator.pop(context);
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    foregroundColor: Colors.grey.shade600,
+                  ),
+                  child: const Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [Icon(Icons.navigate_before), Text('Назад')],
+                  ),
                 ),
-                child: const Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [Icon(Icons.navigate_before), Text('Назад')],
-                ),
-              ),
-            const SizedBox(height: 20),
-          ],
+              const SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
